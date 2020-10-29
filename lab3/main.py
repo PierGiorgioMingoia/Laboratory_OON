7596a786b28b4896bb098e34946a45233d171499
PierGiorgioMingoia <piergiorgio.mingoia@gmail.com>
2020-10-29 17:42:05 +0100

    refactoring

--- lab3/main.py
+++ lab3/main.py
@@ -7,43 +7,68 @@ import pandas as pd
 
 
 class Signal_information:
-    def __init__(self, signal_power, path):
-        self.signal_power = signal_power
-        self.noise_power = 0
-        self.latency = 0
-        self.path = path
+    def __init__(self, signal_power, path, latency=None, noise_power=None):
+        self._signal_power = signal_power
+        if noise_power:
+            self._noise_power = noise_power
+        else:
+            self._noise_power = 0
+        if latency:
+            self._latency = latency
+        else:
+            self._latency = 0
+        self._path = path
+
+    @property
+    def signal_power(self):
+        return self._signal_power
+
+    @property
+    def noise_power(self):
+        return self._noise_power
+
+    @property
+    def latency(self):
+        return self._latency
+
+    @property
+    def path(self):
+        return self._path
 
     def update_signal_power(self, increment):
-        self.signal_power += increment
+        self._signal_power += increment
 
     def update_noise_power(self, increment):
-        self.noise_power += increment
+        self._noise_power += increment
 
     def update_latency(self, increment):
-        self.latency += increment
+        self._latency += increment
 
     def update_path(self):
-        self.path.pop(0)
+        self._path.pop(0)
 
 
 class Node:
     def __init__(self, d):
         for key, value in d.items():
             setattr(self, key, value)
-        self.successive = dict()
+        self._successive = dict()
+
+    @property
+    def successive(self):
+        return self._successive
 
     def propagate(self, signal_information):
-        # TODO check if destination
         if len(signal_information.path) == 1:
             print("END OF PROPAGATION")
         else:
             # find the next line
             line_key = False
-            for key in self.successive:
+            for key in self._successive:
                 if key.endswith(signal_information.path[1]):
                     line_key = key
             if line_key:
-                next_line = self.successive[line_key]
+                next_line = self._successive[line_key]
                 signal_information.update_path()
                 next_line.propagate(signal_information)
             else:
@@ -52,18 +77,29 @@ class Node:
 
 class Line:
     def __init__(self, label, length):
-        self.label = label
-        self.length = length
-        self.successive = dict()
+        self._label = label
+        self._length = length
+        self._successive = dict()
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def length(self):
+        return self._label
+
+    @property
+    def successive(self):
+        return self._successive
 
     def latency_generation(self):
-        speed_of_light = 299792458  # m/s
-        lat = self.length / speed_of_light
+        speed_of_light = 299792458 * 2 / 3  # m/s
+        lat = self._length / speed_of_light
         return lat
 
     def noise_generation(self, signal_power):
-        print(self.length)
-        noise = 1e-3 * signal_power * self.length
+        noise = 1e-3 * signal_power * self._length
         return noise
 
     def propagate(self, signal_information):
@@ -76,34 +112,42 @@ class Line:
 
 class Network:
     def __init__(self, json_file):
-        self.nodes = dict()
-        self.lines = dict()
+        self._nodes = dict()
+        self._lines = dict()
         with open(json_file) as f:
             network_data = json.load(f)
         for key, value in network_data.items():
             node = Node({"label": key, **value})
-            self.nodes[key] = node
+            self._nodes[key] = node
             pos = value['position']
             for n in value['connected_nodes']:
                 label = key + n
                 next_pos = network_data[n]['position']
                 length = distance_nodes(pos, next_pos)
                 line = Line(label, length)
-                self.lines[label] = line
+                self._lines[label] = line
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @property
+    def lines(self):
+        return self._lines
 
     def connect(self):
-        for key in self.nodes:
-            for k in self.lines:
+        for key in self._nodes:
+            for k in self._lines:
                 if k.startswith(key):
-                    self.nodes[key].successive[k] = self.lines[k]
-        for key in self.lines:
-            for k in self.nodes:
+                    self._nodes[key].successive[k] = self._lines[k]
+        for key in self._lines:
+            for k in self._nodes:
                 if key.endswith(k):
-                    self.lines[key].successive[k] = self.nodes[k]
+                    self._lines[key].successive[k] = self._nodes[k]
 
     def find_paths(self, node1, node2):
         visited = {}
-        for key in self.nodes:
+        for key in self._nodes:
             visited[key] = False
         paths = []
         path = []
@@ -117,29 +161,65 @@ class Network:
         if u == d:
             paths.append(path[:])
         else:
-            for k in self.nodes[u].successive:
+            for k in self._nodes[u].successive:
                 if not visited[k[1]]:
                     self.count_path_util(k[1], d, visited, path, paths)
         path.pop()
         visited[u] = False
 
     def propagate(self, signal_information):
-        self.nodes[signal_information.path[0]].propagate(signal_information)
+        self._nodes[signal_information.path[0]].propagate(signal_information)
 
     def draw(self):
         x = []
         y = []
-        for key in self.lines:
+        for key in self._lines:
             # print(key)
-            x_values = [self.nodes[key[0]].position[0], self.nodes[key[1]].position[0]]
-            y_values = [self.nodes[key[0]].position[1], self.nodes[key[1]].position[1]]
+            x_values = [self._nodes[key[0]].position[0], self._nodes[key[1]].position[0]]
+            y_values = [self._nodes[key[0]].position[1], self._nodes[key[1]].position[1]]
             plt.plot(x_values, y_values, linestyle='--', color='r')
-        for key in self.nodes:
+        for key in self._nodes:
             # print(key)
-            x.append(self.nodes[key].position[0])
-            y.append(self.nodes[key].position[1])
+            x.append(self._nodes[key].position[0])
+            y.append(self._nodes[key].position[1])
         plt.plot(x, y, marker='o', color='b', linestyle='')
         plt.show()
+
+    def generate_dataframe(self, signal_power):
+        possible_pairs = all_possible_pairs(self._nodes.keys())
+        list_of_paths = []
+        for pair in possible_pairs:
+            pair_paths = self.find_paths(pair[0], pair[1])
+            list_of_paths.append(pair_paths)
+        all_signal_power = []
+        all_signal_latency = []
+        all_signal_noise = []
+        all_signal_ratio = []
+        all_path = []
+
+        for paths in list_of_paths:
+            for path in paths:
+                # print(path)
+                signal = Signal_information(signal_power, path)
+                string_path = ''
+                for node in path:
+                    string_path += node + '->'
+                string_path = string_path[:-2]
+                all_path.append(string_path)
+                net.propagate(signal)
+                all_signal_power.append(signal.signal_power)
+                all_signal_latency.append(signal.latency)
+                all_signal_noise.append(signal.noise_power)
+                all_signal_ratio.append(signal_to_noise_ratio(signal.signal_power, signal.noise_power))
+
+        df = pd.DataFrame({
+            "Path": all_path,
+            "Total_latency": all_signal_latency,
+            "Total_noise": all_signal_noise,
+            "Signal_noise_ratio": all_signal_ratio
+        })
+
+        return df
 
 
 def distance_nodes(x, y):
@@ -159,50 +239,15 @@ if __name__ == '__main__':
     net = Network('./data/nodes.json')
     net.connect()
     net.find_paths('A', 'B')
-    # net.draw()
+    net.draw()
 
-    signal = Signal_information(0.001, ['B', 'A'])
-    net.propagate(signal)
-    print("Power of the signal: {}".format(signal.signal_power))
-    print("Latency of the signal: {}".format(signal.latency))
-    print("Noise of the signal: {}".format(signal.noise_power))
-    print("Path of the signal: {}".format(signal.path))
+    signal_test = Signal_information(0.001, ['B', 'A'])
+    net.propagate(signal_test)
+    print("Power of the signal: {}".format(signal_test.signal_power))
+    print("Latency of the signal: {}".format(signal_test.latency))
+    print("Noise of the signal: {}".format(signal_test.noise_power))
+    print("Path of the signal: {}".format(signal_test.path))
 
-    possible_pairs = all_possible_pairs(['A', 'B', 'C', 'D', 'E', 'F'])
-    list_of_paths = []
-    for pair in possible_pairs:
-        pair_paths = net.find_paths(pair[0], pair[1])
-        list_of_paths.append(pair_paths)
-    # print(list_of_paths)
-
-    all_signal_power = []
-    all_signal_latency = []
-    all_signal_noise = []
-    all_signal_ratio = []
-    all_path = []
-    for paths in list_of_paths:
-        for path in paths:
-            print(path)
-            signal = Signal_information(0.001, path)
-            string_path = ''
-            for node in path:
-                string_path += node + '->'
-            string_path = string_path[:-2]
-            all_path.append(string_path)
-            net.propagate(signal)
-            all_signal_power.append(signal.signal_power)
-            all_signal_latency.append(signal.latency)
-            all_signal_noise.append(signal.noise_power)
-            all_signal_ratio.append(signal_to_noise_ratio(signal.signal_power, signal.noise_power))
-
-    print(len(all_path))
-    print(len(all_signal_latency))
-
-    df = pd.DataFrame({
-        "Path": all_path,
-        "Total_latency": all_signal_latency,
-        "Total_noise": all_signal_noise,
-        "Signal_noise_ratio": all_signal_ratio
-    })
-
-    print(df.head(5))
+    data_frame = net.generate_dataframe(0.001)
+    print(data_frame.tail(10))
+    data_frame.to_csv('./data/lab3_dataframe.csv')
