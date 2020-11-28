c54b47de11e7ecbb5cdf78021438bf1a0870b144
PierGiorgioMingoia <piergiorgio.mingoia@gmail.com>
2020-11-28 16:23:32 +0100

    some new tests

--- core/main.py
+++ core/main.py
@@ -2,11 +2,11 @@ from classes import *
 import pandas as pd
 
 if __name__ == '__main__':
-    net = Network('./data/nodes_full.json')
+    net = Network('./data/nodes_not_full.json')
     weighted_paths_df = pd.read_csv('./data/lab3_dataframe.csv', index_col=0)
     net.weighted_paths = weighted_paths_df
     connections = test_100_connections(net)
-    histogram_of_connections(connections)
+    # histogram_of_connections(connections)
     # print(net.route_space)
     # print(net.weighted_paths)
     # net.draw()
