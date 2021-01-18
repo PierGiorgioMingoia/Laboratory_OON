ea92b09e6a353b35bbf14f490243217aef7f6e55
PierGiorgioMingoia <piergiorgio.mingoia@gmail.com>
2021-01-18 15:15:56 +0100

    Refactoring and add traffic matrix test

--- core/main.py
+++ core/main.py
@@ -2,32 +2,23 @@ from classes import *
 import pandas as pd
 
 if __name__ == '__main__':
-    net = Network('./data/nodes_full_fixed_rate.json')
+    net = Network('./data/nodes_full_flex_rate.json')
     weighted_paths_df = pd.read_csv('./data/lab10_dataframe.csv', index_col=0)
     net.weighted_paths = weighted_paths_df
+
     # connections = test_100_connections(net)
-
     # snrs, bit_rates = lab8_histograms(connections)
-    # print_bit_rate_and_snr_average_and_total_c(snrs, bit_rates)
-    # connections = []
-    # traffic_matrix = net.create_traffic_matrix(M=5)
-    # for i in range(150):
-    # connections.append(net.creates_handle_connection_from_traffic_matrix(traffic_matrix))
-
-    # print(traffic_matrix)
-    # snrs, bit_rates = lab8_histograms(connections)
-    # print_bit_rate_and_snr_average_and_total_c(snrs, bit_rates)
-    # print(net.route_space)
-    # print(net.weighted_paths)
+    # print_bit_rate_and_snr_average_and_total_c(connections)
     # net.draw()
     # connection = test_2_connection('A', 'F', net)
 
-    traffic_matrix = net.create_traffic_matrix(M=8)
+    traffic_matrix = net.create_traffic_matrix(M=4)
+    display_traffic_matrix(traffic_matrix)
     conn, r, s = net.satisfy_traffic_matrix(traffic_matrix)
     print(len(conn), r, s)
-    snrs, bit_rates = lab8_histograms(conn)
-
-
+    print(traffic_matrix)
+    traffic_matrix_results_plot(conn, r, s)
+    display_traffic_matrix(traffic_matrix)
 
     # df = net.generate_dataframe(0.001)
     # df.to_csv('./data/lab10_dataframe.csv')
